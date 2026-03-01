955dc9a9e42bbd6cd2111f9777ae2e59ad311fb3
KAMESH-RAVICHANDRAN <kamesh6592@gmail.com>
2026-03-01 21:44:22 +0530

    fix: disable CDN asset prefix when SITE_NAME is not set (fixes blank page on custom Vercel deploys)

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -8,13 +8,18 @@ const { getCSP } = require('./csp')
 const path = require('path')
 
 function getAssetPrefix() {
+  // Force disable CDN
+  if (process.env.FORCE_ASSET_CDN === '-1') {
+    return undefined
+  }
+
   // If not force enabled, but not production env, disable CDN
   if (process.env.FORCE_ASSET_CDN !== '1' && process.env.VERCEL_ENV !== 'production') {
     return undefined
   }
 
-  // Force disable CDN
-  if (process.env.FORCE_ASSET_CDN === '-1') {
+  // Disable CDN if required env vars are missing (self-hosted / custom Vercel deploys)
+  if (!process.env.SITE_NAME || !process.env.VERCEL_GIT_COMMIT_SHA) {
     return undefined
   }
 
